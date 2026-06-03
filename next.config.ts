e2fa85964d4b6879debc9bfc98312f848e679369
Hugo Valet <hugo@MacBook-Air-de-Hugo.local>
2026-06-03 13:43:27 +0200

    fix: disable turbopack

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // Autorise ton iPhone à charger les fichiers Next.js en dev
-  allowedDevOrigins: ["192.168.1.201"],
-};
-
-export default nextConfig;
