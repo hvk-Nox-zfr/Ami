e04a3acd34e619766a66a2672ec4c8c064001e60
Hugo Valet <hugo@MacBook-Air-de-Hugo.local>
2026-06-03 20:19:57 +0200

    fix: disable turbopack

--- lib/pusher.ts
+++ lib/pusher.ts
@@ -1,18 +1,41 @@
 import Pusher from "pusher";
 import PusherClient from "pusher-js";
 
-export const serverPusher = new Pusher({
-  appId: process.env.PUSHER_APP_ID!,
-  key: process.env.PUSHER_KEY!,
-  secret: process.env.PUSHER_SECRET!,
-  cluster: process.env.PUSHER_CLUSTER!,
-  useTLS: true,
-});
+// ----------------------
+// SERVER PUSHER (API)
+// ----------------------
+let serverPusher: Pusher | null = null;
 
-// IMPORTANT : utiliser NEXT_PUBLIC_ pour le client
-export const clientPusher = new PusherClient(
-  process.env.NEXT_PUBLIC_PUSHER_KEY!,
-  {
-    cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
-  }
-);
+if (
+  process.env.PUSHER_APP_ID &&
+  process.env.PUSHER_KEY &&
+  process.env.PUSHER_SECRET &&
+  process.env.PUSHER_CLUSTER
+) {
+  serverPusher = new Pusher({
+    appId: process.env.PUSHER_APP_ID,
+    key: process.env.PUSHER_KEY,
+    secret: process.env.PUSHER_SECRET,
+    cluster: process.env.PUSHER_CLUSTER,
+    useTLS: true,
+  });
+}
+
+export { serverPusher };
+
+// ----------------------
+// CLIENT PUSHER (Browser)
+// ----------------------
+let clientPusher: PusherClient | null = null;
+
+if (
+  typeof window !== "undefined" &&
+  process.env.NEXT_PUBLIC_PUSHER_KEY &&
+  process.env.NEXT_PUBLIC_PUSHER_CLUSTER
+) {
+  clientPusher = new PusherClient(process.env.NEXT_PUBLIC_PUSHER_KEY, {
+    cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
+  });
+}
+
+export { clientPusher };
