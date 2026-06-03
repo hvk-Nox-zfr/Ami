e99dd3feb5a71ba3d4d52fbc3f3fa94a2dc9bf94
Hugo Valet <hugo@MacBook-Air-de-Hugo.local>
2026-06-03 20:11:25 +0200

    fix: disable turbopack

--- app/groups/page.tsx
+++ app/groups/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useState } from "react";
 import CreateGroup from "@/components/CreateGroup";
 import GroupList from "@/components/GroupList";
