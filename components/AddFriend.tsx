ad7a7c0f0e0b8c2143f96f3b0ac3bfdb170ab5f8
Hugo Valet <hugo@MacBook-Air-de-Hugo.local>
2026-06-04 20:30:10 +0200

    fix: disable turbopack

--- components/AddFriend.tsx
+++ components/AddFriend.tsx
@@ -16,7 +16,7 @@ export default function AddFriend() {
       return;
     }
 
-    const res = await fetch("app/api/users/search", {
+    const res = await fetch("/api/users/search", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username: value }),
