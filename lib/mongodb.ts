910d7625e859643ae860ad84cc8ea1bcaeeed534
Hugo Valet <hugo@MacBook-Air-de-Hugo.local>
2026-06-08 12:42:05 +0200

    fix: disable turbopack

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,19 +1,26 @@
-import { MongoClient } from "mongodb";
+import mongoose from "mongoose";
 
-const uri = process.env.MONGODB_URI!;
-if (!uri) throw new Error("❌ MONGODB_URI manquant dans .env");
+// On force TypeScript à savoir que la variable existe
+const uri = process.env.MONGODB_URI as string;
 
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
-
-// @ts-ignore
-if (!global._mongoClientPromise) {
-  client = new MongoClient(uri);
-  // @ts-ignore
-  global._mongoClientPromise = client.connect();
+if (!uri) {
+  throw new Error("❌ MONGODB_URI manquant dans .env");
 }
 
-// @ts-ignore
-clientPromise = global._mongoClientPromise;
+// Cache global pour éviter les connexions multiples
+let cached = (global as any).mongoose;
 
-export default clientPromise;
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+export default async function connect() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(uri).then((m) => m);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
