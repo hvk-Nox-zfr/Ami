d331adef2571484f79765959363a7321cdd11913
Hugo Valet <hugo@MacBook-Air-de-Hugo.local>
2026-06-10 14:18:20 +0200

    fix: bug fix

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,10 +2,14 @@ export const dynamic = "force-dynamic";
 
 import ClientLayout from "../ClientLayout";
 import HomeClientWrapper from "./HomeClientWrapper";
+import PushClient from "../push-client"; // ← ajoute ça
 
 export default function Page() {
+  const username = "TON_USERNAME_ICI"; // ou récupéré depuis ton auth
+
   return (
     <ClientLayout>
+      <PushClient username={username} />   {/* ← ajoute ça */}
       <HomeClientWrapper />
     </ClientLayout>
   );
